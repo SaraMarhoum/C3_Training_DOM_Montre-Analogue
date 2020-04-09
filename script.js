a75d679be34c4529eddf270f52f21be331fa37db
SaraMarhoum <57989282+SaraMarhoum@users.noreply.github.com>
2020-04-09 16:19:59 +0100

    activer la montre

--- script.js
+++ script.js
@@ -3,22 +3,32 @@ const AIGUILLEHR = document.querySelector("#hour");
 const AIGUILLEMIN = document.querySelector("#minute");
 const AIGUILLESEC = document.querySelector("#second");
 
-//Extraire l'heure actuel à l'aide de l'objet Date()
-
-
-//Ajouter l'heure , minite , seconde  dans des varaiables
-
-
-
-// Calculer de degré de mouvement de l'aiguille heure, de l'aiguille minute, de l'aiguille seconde
-// Hint : Tous les aiguilles doivent se déplacer chaque second selon un degré
 
 
 // Déplacer les aiguilles 
 function demarrerLaMontre() {
 
+    //Extraire l'heure actuel à l'aide de l'objet Date()
+    const actualDate = new Date();
 
+    //Ajouter l'heure , minite , seconde  dans des varaiables
+    var heure = actualDate.getHours();
+    var minute = actualDate.getMinutes();
+    var seconde = actualDate.getSeconds();
+
+    // Calculer de degré de mouvement de l'aiguille heure, de l'aiguille minute, de l'aiguille seconde
+     // Hint : Tous les aiguilles doivent se déplacer chaque second selon un degré
+     // 360° dans le rond & 60s/min & 1min = 360° ==> on veut ° pour every sec
+    const secDeg = ((seconde / 60) * 360);
+    const minDeg = ((minute / 60) * 360);
+    const hrDeg = ((heure / 60) * 360);
+
+    // Déplacer les aiguilles 
+    AIGUILLESEC.style.transform=`rotate(${secDeg}deg)`
+    AIGUILLEMIN.style.transform=`rotate(${minDeg}deg)`
+    AIGUILLEHR.style.transform=`rotate(${hrDeg}deg)`
 
 }
+
 // Exercuter la fonction chaque second
 var interval = setInterval(demarrerLaMontre, 1000);
