82df6afe6a7cbea288ac4aabf0b511153c6e7aa6
SaraMarhoum <57989282+SaraMarhoum@users.noreply.github.com>
2020-04-09 16:25:20 +0100

    set seconds and rotate

--- script.js
+++ script.js
@@ -12,21 +12,21 @@ function demarrerLaMontre() {
     const actualDate = new Date();
 
     //Ajouter l'heure , minite , seconde  dans des varaiables
-    var heure = actualDate.getHours();
-    var minute = actualDate.getMinutes();
+    // var heure = actualDate.getHours();
+    // var minute = actualDate.getMinutes();
     var seconde = actualDate.getSeconds();
 
     // Calculer de degré de mouvement de l'aiguille heure, de l'aiguille minute, de l'aiguille seconde
      // Hint : Tous les aiguilles doivent se déplacer chaque second selon un degré
      // 360° dans le rond & 60s/min & 1min = 360° ==> on veut ° pour every sec
     const secDeg = ((seconde / 60) * 360);
-    const minDeg = ((minute / 60) * 360);
-    const hrDeg = ((heure / 60) * 360);
+    // const minDeg = ((minute / 60) * 360);
+    // const hrDeg = ((heure / 60) * 360);
 
     // Déplacer les aiguilles 
     AIGUILLESEC.style.transform=`rotate(${secDeg}deg)`
-    AIGUILLEMIN.style.transform=`rotate(${minDeg}deg)`
-    AIGUILLEHR.style.transform=`rotate(${hrDeg}deg)`
+    // AIGUILLEMIN.style.transform=`rotate(${minDeg}deg)`
+    // AIGUILLEHR.style.transform=`rotate(${hrDeg}deg)`
 
 }
 
